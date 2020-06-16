817c11149449102bcff31554b7923f22f279dba0
Anya Vigolajnen <avi26.job@yandex.ru>
2020-06-16 20:56:51 +0300

    nav-links, отступы item, gallery flex

--- js/myGallery.js
+++ js/myGallery.js
@@ -1,10 +1,11 @@
-var gallery = document.querySelector('.gallery');
-if (gallery) {
-  var msnry = new Masonry( '.gallery__wrapper', {
-    // options
-    itemSelector: '.gallery__item',
-    columnWidth: '.gallery__sizer',
-    gutter: '.gallery__gutter-sizer',
-    percentPosition: true
-  });
-}
+// var gallery = document.querySelector('.gallery');
+// if (gallery) {
+//   var msnry = new Masonry( '.gallery__wrapper', {
+//     // options
+//     itemSelector: '.gallery__item',
+//     columnWidth: '.gallery__sizer',
+//     gutter: '.gallery__gutter-sizer',
+//     percentPosition: true,
+//     horizontalOrder: false
+//   });
+// }
